2ceea62dd9ff2c1389a32bf34a8a8afdccfe31b7
nasserbek <54413843+nasserbek@users.noreply.github.com>
2020-11-13 13:24:24 +0100

    v16

--- bek.ino
+++ bek.ino
@@ -125,12 +125,12 @@ void netgeerCtrl(void)
               {
                 pingGoogle = pingGoogleConnection();
                 blynkConnected = myBlynk.blynkActive();
-                if (blynkConnected || pingGoogle) { restartAfterResetNG = millis();netGeerReset = false;}
+                if (blynkConnected) { restartAfterResetNG = millis();netGeerReset = false;}
                 DEBUG_PRINT("Blynk Connection: ");DEBUG_PRINTLN(blynkConnected ? F("succesiful") : F("failed"));
                 internetSurvilanceTimer= millis();
               }
               
-       if (!blynkConnected && !pingGoogle &&!netGeerReset )  
+       if (!blynkConnected && !netGeerReset )  
             { 
               sim.SendSMS("Reset Netgeer for Internet Failure");
               DEBUG_PRINTLN("Reset Netgeer for Internet Failure");
--- headers.h
+++ headers.h
@@ -1,5 +1,5 @@
 #define BEK
-#define VERSION_ID " V15.0 CREIL "
+#define VERSION_ID " V16.0 CREIL "
 #define DEBUG_SERIAL
 
 
