12ec0d94acdfbd6733887d0ddeb0239fab002d26
nasserbek <54413843+nasserbek@users.noreply.github.com>
2021-01-08 14:59:55 +0100

    60 1

--- blynk_app.cpp
+++ blynk_app.cpp
@@ -116,7 +116,7 @@ void myfunction(){
     Blynk.virtualWrite(V21, minutes);
     Blynk.virtualWrite(V11, seconds);
     Serial.println("\tTick update to blynk.");
-    if(firstConnect== false) {firstConnect= true;Blynk.syncAll();}
+  //  if(firstConnect== false) {firstConnect= true;Blynk.syncAll();}
     
     _blynkIsConnected = true;
   }
--- headers.h
+++ headers.h
@@ -1,5 +1,5 @@
 #define BEK
-#define VERSION_ID " V60.0 "
+#define VERSION_ID " V60.1 "
 #define DEBUG_SERIAL
 #define BLYNK_SERVER "blynk-cloud.com"
 
