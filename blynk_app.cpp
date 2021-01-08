16016a3d0223bde89664b00b4ee834a950427a7b
nasserbek <54413843+nasserbek@users.noreply.github.com>
2021-01-08 23:09:23 +0100

    64

--- blynk_app.cpp
+++ blynk_app.cpp
@@ -913,6 +913,6 @@ bool blynk::wifiStatus(void)
 
 void blynk::streamSelect(bool stream)
 {
-  if (stream == DDNS) Blynk.setProperty(V4,"url", "rtsp://creil:basma28112018@bouy.ddns.net:5001/ch01/0");
-  else if (stream == WEB) Blynk.setProperty(V4, "url","rtsp://creil:basma28112018@192.168.1.94:554/ch01/0");
+  if (stream == DDNS) Blynk.setProperty(V22,"url", "rtsp://creil:basma28112018@bouy.ddns.net:5001/ch01/0");
+  else if (stream == WEB) Blynk.setProperty(V22, "url","rtsp://creil:basma28112018@192.168.1.94:554/ch01/0");
 }
