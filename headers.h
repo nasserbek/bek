e7da8fd62b03b8d3580c6510f52248b59316298a
nasserbek <nasserbek@yahoo.it>
2022-04-07 15:50:55 +0200

    V

--- headers.h
+++ headers.h
@@ -4,7 +4,7 @@
 #define PI4
 
 #define FREE
-#define VERSION_ID " Va4.03 " //Vxy.zz  x=a Main, b Spare  y=4 PI4 , 3 PI3 , 0 PI0 zz version
+#define VERSION_ID " Vm4.pi_00 " //Vxy.zz  x=m Main, s Spare  y=4 PI4 , 3 PI3 , 0 PI0 zz pi or pc version
 #define DEBUG_SERIAL
 
 
