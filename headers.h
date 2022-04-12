757631cdde486bb1738c7636197f0fcac8061c16
nasserbek <54413843+nasserbek@users.noreply.github.com>
2022-04-12 15:48:37 +0200

    cc

--- headers.h
+++ headers.h
@@ -31,20 +31,20 @@
 #endif
 
 
-#ifdef BLYNK_REMOTE_SERVER
-    
-      #ifdef MAIN_BOARD
-                #define BLYNK_AUTH "D4AU1HexWcErQ9vtpkP_EgocpnoArZKC" //MAIN BOX
-      #else
-                #define BLYNK_AUTH "ya1T2eipkMhB3NvyLeAyRVRHqPAUXUG-"  //SPARE BOX
-      #endif   
-#else    
+//#ifdef BLYNK_REMOTE_SERVER
+//    
+//      #ifdef MAIN_BOARD
+//                #define BLYNK_AUTH "D4AU1HexWcErQ9vtpkP_EgocpnoArZKC" //MAIN BOX
+//      #else
+//                #define BLYNK_AUTH "ya1T2eipkMhB3NvyLeAyRVRHqPAUXUG-"  //SPARE BOX
+//      #endif   
+//#else    
       #ifdef MAIN_BOARD
                 #define BLYNK_AUTH "mnjUdg63gkJ0Rl7Pbfw2qb9aGkI35wPJ" //MAIN BOX
       #else
                 #define BLYNK_AUTH "cfefMEczv_cZVHomzDd4uCbY5CRL8X5m"  //SPARE BOX
       #endif                                            
-#endif  
+//#endif  
 
 
 
