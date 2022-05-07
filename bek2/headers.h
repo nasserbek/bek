1ba8b55fcb750f821ea9b294c029265933228578
nasserbek <54413843+nasserbek@users.noreply.github.com>
2022-05-07 14:21:53 +0200

    s2

--- bek2/headers.h
+++ bek2/headers.h
@@ -10,7 +10,7 @@
 //#define OPPO
 #define GIGACUBE
 
-#define VERSION_ID " VSGB.140422 " //VxyW.zz  x=m Main, s Spare  y=F FREE N NETGEER W =B BLYNK zz pi or pc version
+#define VERSION_ID " VSGB.070522 " //VxyW.zz  x=m Main, s Spare  y=F FREE N NETGEER W =B BLYNK zz pi or pc version
 
    
 
