397d9626f3fb6084d4d5c119520625f82e2ce8c5
nasserbek <54413843+nasserbek@users.noreply.github.com>
2022-05-24 18:24:20 +0200

    rrr

--- bek.ino
+++ bek.ino
@@ -689,23 +689,23 @@ void processBlynkQueu(void)
 
             
             case Q_EVENT_ZAP_CHANNEL16_V108 :
-              videoCh[16].zap=fb.eventValue;
+              videoCh[16].zap=true ;//fb.eventValue;
             break; 
 
              case Q_EVENT_ZAP_CHANNEL17_V109 :
-              videoCh[17].zap=fb.eventValue;
+              videoCh[17].zap=true ;//fb.eventValue;
             break; 
 
             case Q_EVENT_ZAP_CHANNEL18_V110 :
-              videoCh[18].zap=fb.eventValue;
+              videoCh[18].zap=true ;//fb.eventValue;
             break;             
 
              case Q_EVENT_ZAP_CHANNEL19_V111 :
-              videoCh[19].zap=fb.eventValue;
+              videoCh[19].zap=true ;//fb.eventValue;
             break; 
  
              case Q_EVENT_ZAP_CHANNEL20_V112 :
-              videoCh[20].zap=fb.eventValue;
+              videoCh[20].zap=true ;//fb.eventValue;
             break; 
                        
             
