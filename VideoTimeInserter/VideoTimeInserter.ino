e1b707c0554e81fa6c90e5a9d126e3f548f1b0f1
nasserbek <54413843+nasserbek@users.noreply.github.com>
2023-07-31 23:26:11 +0200

    RC

--- VideoTimeInserter/VideoTimeInserter.ino
+++ VideoTimeInserter/VideoTimeInserter.ino
@@ -58,7 +58,7 @@
   //  Max7456 SCK   [SCLK]           <---  Mega2560 52  [PB1](SCK)
   const byte slaveClock                =                      SCK;
   //  Max7456 RST   [~RESET]          ---  Arduino  RST      (RESET)
-  const byte osdReset                  =            0;
+  const byte osdReset                  =            15;
   //  Max7456 VSYNC [~VSYNC]          -X-
   //  Max7456 HSYNC [~HSYNC]          -X-
   //  Max7456 LOS   [LOS]             -X-
@@ -137,131 +137,15 @@
   void loop() 
   {
     char room[] = "213";
-    char date[] = "2012-01-01";
-    char time[] = "00:00:00.000";
-    
-    int year = (date[0] - '0') * 1000
-                 + (date[1] - '0') * 100
-                 + (date[2] - '0') * 10
-                 + (date[3] - '0');
-    byte leap = isLeapYear(year);
-    byte month = (date[5] - '0') * 10
-                   + (date[6] - '0');
-    byte day = (date[8] - '0') * 10
-                 + (date[9] - '0');
-    int msOffset = (time[11] - '0') * 100
-                     + (time[10] - '0') * 10
-                     + (time[9] - '0');
-    char daysPerMonth[2][12] = { 
-               { 31, 28, 31, 30, 31, 30, 
-                 31, 31, 30, 31, 30, 31  },
-               { 31, 29, 31, 30, 31, 30, 
-                 31, 31, 30, 31, 30, 31  }
-             };
-    bool toggle = false;
-    
-    
-    while (true)
-    {
-      
-      while (OSD.notInVSync()) {
-
-        time[11] = (millis() + msOffset)        // The millis() function keeps 
-                     % 10 + '0';                //   the time even when not
-        time[10] = (millis() + msOffset)        //   calculating it.
-                     / 10 % 10 + '0';
-        time[9] = (millis() + msOffset)
-                     / 100 % 10 + '0';
-
-        if (!toggle && time[9] == '5') {        // Converts millis() into a 
-          toggle = true;                        //   pulse per second.
-        }
-        if (toggle && time[9] == '0') {
-          time[7]++;                            // Increment on falling edge.
-          toggle = false;
-        }
-        
-        if(time[7] > '9') {
-          time[7] = '0';
-          time[6]++;
-        }
-        if(time[6] > '5') {
-          time[6] = '0';
-          time[4]++;
-        }
-        if (time[4] > '9') {
-          time[4] = '0';
-          time[3]++;
-        }
-        if (time[3] > '5') {
-          time[3] = '0';
-          time[1]++;
-        }
-        if (time[1] > '9') {
-          time[1] = '0';
-          time[0]++;
-        }
-        if (time[0] >= '2' && time[1] > '3') {
-          time[1] = '0';
-          time[0] = '0';
-          date[9]++;
-          day++;
-        }
-        
-        if (date[9] > '9') {
-          date[9] = '0';
-          date[8]++;
-        }
-        
-        if (day > daysPerMonth[leap][month - 1]) {
-          date[9] = '1';
-          date[8] = '0';
-          day = 1;
-          date[6]++;
-          month++;
-        }
-        if (date[6] > '9') {
-          date[6] = '0';
-          date[5]++;
-        }
-        if (date[5] >= '1' && date[6] > '2') {
-          date[6] = '1';
-          date[5] = '0';
-          month = 1;
-          date[3]++;
-          year++;
-          leap = isLeapYear(year);
-        }
-        if (date[3] > '9') {
-          date[3] = '0';
-          date[2]++;
-        }
-        if (date[2] > '9') {
-          date[2] = '0';
-          date[1]++;
-        }
-        if (date[1] > '9') {
-          date[1] = '0';
-          date[0]++;
-        }
-        if (date[0] > '9') {
-          date[0] = '0';
-        }
-        
-        // Add aditional code here.
-      }
-      
+   
       OSD.setCursor( 0, -1 );
       OSD.print( room );
-      //OSD.setCursor( 0 - strlen(time), -1 );
-      //OSD.print( time );
-      
+     
       while (!OSD.notInVSync())
       {
         // Add aditional code here.
       }
-    }
-    
+        
   } 
   // loop()
 
