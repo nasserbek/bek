1b9ddd76690055adfe957046a688627ef206cdff
nasserbek <54413843+nasserbek@users.noreply.github.com>
2025-10-12 03:22:39 +0200

    v13

--- WORK/CSR/blynk_app.cpp
+++ WORK/CSR/blynk_app.cpp
@@ -281,8 +281,8 @@ void blynk::mapRefresh(int index)
 
 void blynk::streamSelect(bool stream)
 {
-  if (stream == DDNS) Blynk.setProperty(V28,"url", "rtsp://sdp:basma28112018@evrx.ddns.net:5001/ch01/0");
-  else if (stream == WEB) Blynk.setProperty(V28, "url","rtsp://sdp:basma28112018@192.168.1.94:554/ch01/0");
+  if (stream == DDNS) Blynk.setProperty(V28,"url", "rtsp://admin:basma28112018@evrx.ddns.net:5001/ch02/0");
+  else if (stream == WEB) Blynk.setProperty(V28, "url","rtsp://admin:basma28112018@192.168.1.96:554/ch02/0");
 }
 
 
